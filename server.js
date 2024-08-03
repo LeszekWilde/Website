02de29efe650ef100b2c28961ea18eb48104db20
LeszekWilde <146771084+LeszekWilde@users.noreply.github.com>
2024-08-03 13:51:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,23 +7,4 @@ const app = express();
 const HOST = '94.237.84.98';
 const PORT = 80;
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
-
-app.post('/save-contact', (req, res) => {
-    const { name, email, message } = req.body;
-    const contactInfo = `Name: ${name}\nEmail: ${email}\nMessage: ${message}\n\n`;
-    
-    fs.appendFile('contacts.txt', contactInfo, (err) => {
-        if (err) {
-            console.error(err);
-            res.status(500).send('Internal Server Error');
-        } else {
-            res.send('Contact information saved successfully!');
-        }
-    });
-});
-
-app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
-});
+app.use(bodyParser.urlencoded({ extended
